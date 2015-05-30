3413f610107364a086a1753f9208c6a586bdda4a
David Tebbi <davidtebbi@gmail.com>
2015-05-30 13:57:26 -0400

    finish fql

--- fql.js
+++ fql.js
@@ -30,19 +30,35 @@ FQL.prototype.limit = function(lim) {
 
 FQL.prototype.where = function(whereObj) {
 	var results = [];
-
-	for (var j = 0; j < this.data.length; j++) {
-		var keep = true;
-		for(key in whereObj) {
-			if(typeof whereObj[key] !== "function") {
-				if (this.data[j][key] !== whereObj[key]) {
-					keep = false;
+	var unindexed = this.data;
+	if(this.indexTable === undefined) {
+		for (var j = 0; j < this.data.length; j++) {
+			var keep = true;
+			for(key in whereObj) {
+				if(typeof whereObj[key] !== "function") {
+					if (this.data[j][key] !== whereObj[key]) {
+						keep = false;
+					}
+				} else if (!whereObj[key](this.data[j][key])) {
+						keep=false;
+					}
 				}
-			} else if (!whereObj[key](this.data[j][key])) {
-					keep=false;
+			if(keep) { results.push(this.data[j]) }
+		}
+	} else {
+		for(idxName in this.indexTable) {
+			for(key in whereObj) {
+				if(key === idxName) {
+					// console.log("where");
+					//console.log(this.getIndicesOf(key, whereObj[key]));
+					var resultsArr = this.getIndicesOf(key, whereObj[key]);
+					//console.log(resultsArr);
+					results = resultsArr.map(function(val) {
+						return unindexed[val];
+					})
 				}
 			}
-		if(keep) { results.push(this.data[j]) }
+		}
 	}
 	return new FQL(results);
 };
@@ -66,3 +82,76 @@ FQL.prototype.order = function(key) {
 	};
 	return new FQL(this.data.sort(compare));
 };
+
+FQL.prototype.left_join = function(joinTable, joinFunc) {
+	var results = [];
+	for (var j = 0; j < this.data.length; j++) {
+		for(i=0; i < joinTable.data.length; i++) {
+			if(joinFunc(this.data[j],joinTable.data[i])) {
+				results.push(merge(this.data[j],joinTable.data[i]));
+			}
+		}
+	}
+	return new FQL(results);
+};
+
+FQL.prototype.addIndex = function(colName) {
+	var attr = this.select(colName.split(","));
+	//console.log(attr);
+	results = {};
+	this.indexTable = {};
+	this.indexTable[colName] = {};
+	for(var idx in attr.data) {
+		if(!results[attr.data[idx][colName]]){
+			results[attr.data[idx][colName]] = [];
+		}
+		results[attr.data[idx][colName]].push(parseInt(idx));
+	}
+
+	this.indexTable[colName] = results;
+	//console.log(this);
+	return this;
+
+	// var sourceTable = this.exec();
+	// var i = 0;
+	// var indexTable = {};
+	// sourceTable.forEach( function(sourceObj) {
+	// 	var colName = sourceObj[indexCol]
+	// 	if (!indexTable[colName]){
+	// 		indexTable[colName] = []
+	// 	}
+	// 	indexTable[colName].push(i)
+	// 	i++
+	// })
+	// this.indexTable = indexArray
+};
+
+FQL.prototype.getIndicesOf = function(colName, colVal) {
+	if(!this.indexTable || !this.indexTable[colName]) {
+		return undefined;
+	}
+	return this.indexTable[colName][colVal];
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
